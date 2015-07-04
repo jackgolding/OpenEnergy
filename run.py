b2b6a3bbe2a8fed778e8bf2a232816f27684bf6c
Jack Golding <jackgolding@live.com.au>
2015-07-04 22:25:49 +0800

    Config for production

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 
 from app import app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
